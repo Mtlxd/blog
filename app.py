d4afa83235718d67aaa1ab6304c6ed24555c1180
Mtlxd <giovani.orlando@hotmail.com>
2022-07-26 23:18:26 +0000

    git

--- app.py
+++ app.py
@@ -3,17 +3,28 @@
 #pip install -r requirements.txt (carrega as configurações do requeriments)
 #Ferramenta de Dev no browser CTRL+SHIFT+I
 from flask import Flask, render_template #bibliotecas usadas
-
+from datetime import datetime
 app = Flask('hello')
+
+posts = [
+    {
+        'title': 'O meu Primeiro Post',
+        'body' : 'Aqui é o texto do Post',
+        'author': 'Feulo',
+        'created': datetime(2022,7,25)
+    },
+    {
+        'title': 'O meu segundo Post',
+        'body' : 'Aqui é o texto do Post',
+        'author': 'Danilo',
+        'created': datetime(2022,7,26)
+    },
+
+
+]
 
 @app.route('/') #Cria uma rota para a aplicação (raiz da aplicação)
 #Route permite que a gente atribua uma função
 #o '@'é uma anotação para rota
-@app.route('/hello')#Rota do servidor
-def hello():
-    return 'Hello World!' #flask run (para rodar)
-
-@app.route('/meucontato')#Rota do servidor
-def meuContato():
-    return render_template('index.html')
-    
+def index():
+    return render_template('index.html', posts=posts) #flask run (para rodar)
